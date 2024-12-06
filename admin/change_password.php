2c838c4153237464c955dcaa76925f533d7f2a01
Rodrigo Sambade <43375046+rodrigosambadesaa@users.noreply.github.com>
2024-12-06 18:13:28 +0100

    Improve security

--- admin/change_password.php
+++ admin/change_password.php
@@ -59,6 +59,16 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
                 $update_stmt->execute();
 
                 echo "<p style='color: green;'>Contraseña actualizada correctamente.</p>";
+                echo "<p><strong>Consejos para mantener tus contraseñas segura:</strong></p>";
+                echo "<ul>";
+                echo "<li>Utiliza una contraseña única para cada cuenta.</li>";
+                echo "<li>Utiliza una combinación de letras mayúsculas, minúsculas, números y caracteres especiales, y al menos 12 caracteres de longitud.</li>";
+                echo "<li>No compartas tu contraseña con nadie.</li>";
+                echo "<li>No guardes tus contraseñas en un lugar visible o de fácil acceso, como en un post-it en tu escritorio o pegado a tu monitor.</li>";
+                echo "<li>No uses información personal en tu contraseña, como tu nombre, fecha de nacimiento, nombre de tu mascota, DNI, etc, ni de tus amigos o familiares o información que hayas compartido en redes sociales o en otro lugar público de Internet o de fuera de Internet.</li>";
+                echo "<li>No uses contraseñas comunes o fáciles de adivinar, como '123456', 'password', 'qwerty', 'abc123', 'admin', 'root', '1234', 'letmein', 'welcome', 'login', 'princess ', 'sunshine";
+                echo "<li><strong>En este sitio se hacen verificar la fortaleza de la contraseña y se comprueba si ha sido filtrada en brechas de seguridad. Pero esto no indica que se haga en otros sitios, por lo que es importante que sigas estos consejos en todos los sitios donde tengas una cuenta.</strong></li>";
+                echo "</ul>";
             } else {
                 echo "<p style='color: red;'>La contraseña actual no es correcta.</p>";
             }
@@ -73,6 +83,16 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
                 $update_stmt->execute();
 
                 echo "<p style='color: green;'>Contraseña actualizada correctamente.</p>";
+                echo "<p><strong>Consejos para mantener tus contraseñas segura:</strong></p>";
+                echo "<ul>";
+                echo "<li>Utiliza una contraseña única para cada cuenta.</li>";
+                echo "<li>Utiliza una combinación de letras mayúsculas, minúsculas, números y caracteres especiales, y al menos 12 caracteres de longitud.</li>";
+                echo "<li>No compartas tu contraseña con nadie.</li>";
+                echo "<li>No guardes tus contraseñas en un lugar visible o de fácil acceso, como en un post-it en tu escritorio o pegado a tu monitor.</li>";
+                echo "<li>No uses información personal en tu contraseña, como tu nombre, fecha de nacimiento, nombre de tu mascota, DNI, etc, ni de tus amigos o familiares o información que hayas compartido en redes sociales o en otro lugar público de Internet o de fuera de Internet.</li>";
+                echo "<li>No uses contraseñas comunes o fáciles de adivinar, como '123456', 'password', 'qwerty', 'abc123', 'admin', 'root', '1234', 'letmein', 'welcome', 'login', 'princess ', 'sunshine";
+                echo "<li><strong>En este sitio se hacen verificar la fortaleza de la contraseña y se comprueba si ha sido filtrada en brechas de seguridad. Pero esto no indica que se haga en otros sitios, por lo que es importante que sigas estos consejos en todos los sitios donde tengas una cuenta.</strong></li>";
+                echo "</ul>";
             } else {
                 echo "<p style='color: red;'>La contraseña actual no es correcta.</p>";
             }
