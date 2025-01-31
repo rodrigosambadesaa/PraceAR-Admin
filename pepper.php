c0790e0360cacfd8e96977dd95fb465c84a223fd
Rodrigo Sambade <43375046+rodrigosambadesaa@users.noreply.github.com>
2025-01-31 11:09:53 +0100

    Updates

--- pepper.php
+++
@@ -1,6 +0,0 @@
-<?php
-// Archivo protegido con permisos muy restrictivos
-return [
-    // Secure pepper with 30 characters, at least one uppercase letter, one lowercase letter, one number, and one special character
-    'PASSWORD_PEPPER' => 'a3C&+/!@#,{]b5M1z8Q2w9E0r4T7y6U',
-];
