0eb1395e5769beb866e84f8dbb1d73505d1c75eb
Rodrigo Sambade <43375046+rodrigosambadesaa@users.noreply.github.com>
2025-10-21 12:23:22 +0200

    Refactor code structure for improved readability and maintainability

--- admin/components/sections/header.php
+++ admin/components/sections/header.php
@@ -10,16 +10,16 @@ require_once(HELPERS . 'get_language.php');
                 <img class="language-flag current-language-flag" width="15" height="15"
                     src="<?= FLAG_IMAGES_URL . (get_language()) . ".png" ?>" alt="<?= get_language() ?>" tabindex="0">
             </strong>
+            <div class="admin-header__darkmode">
+                <button id="toggle-darkmode" aria-label="Cambiar modo oscuro"
+                    style="background:none;border:none;cursor:pointer;font-size:1.5rem;">
+                    <span id="darkmode-icon" title="Cambiar modo oscuro">🌙</span>
+                </button>
+            </div>
         </h1>
         <?php require_once(COMPONENT_ADMIN . "languages.php"); ?>
     </div>
     <div id="cabecera-menu-navegacion" class="admin-header__menu">
         <?php require_once(COMPONENT_ADMIN . "main_menu.php"); ?>
-    </div>
-    <div class="admin-header__darkmode">
-        <button id="toggle-darkmode" aria-label="Cambiar modo oscuro"
-            style="background:none;border:none;cursor:pointer;font-size:1.5rem;">
-            <span id="darkmode-icon" title="Cambiar modo oscuro">🌙</span>
-        </button>
     </div>
 </header>
